e5cdac00b590a5fb98be73e66836cb392863e8fa
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-06-10 12:39:22 +0530

    created an account and connection

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,12 @@
 import { expect, test } from "bun:test";
+import { Connection, Keypair } from "@solana/web3.js";
 
-test("Account is initialised", () => {
-  expect(1).toBe(1);
+let adminAccount = Keypair.generate();
+let dataAccount = Keypair.generate();
+
+test("Account is initialised", async () => {
+  const connection = new Connection("http://127.0.0.1:8899");
+  const data = await connection.getAccountInfo(adminAccount.publicKey);
+  //   expect(1).toBe(1);
+  console.log(data);
 });
