28e06dc769d4485f68fc43a9f6340c4cccf9b1c7
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-06-10 16:24:43 +0530

    we airdropped

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -6,7 +6,12 @@ let dataAccount = Keypair.generate();
 
 test("Account is initialised", async () => {
   const connection = new Connection("http://127.0.0.1:8899");
-  const data = await connection.getAccountInfo(adminAccount.publicKey);
   //   expect(1).toBe(1);
+  const txn = await connection.requestAirdrop(
+    adminAccount.publicKey,
+    1 * 1000_000_000
+  );
+  await connection.confirmTransaction(txn);
+  const data = await connection.getAccountInfo(adminAccount.publicKey);
   console.log(data);
 });
